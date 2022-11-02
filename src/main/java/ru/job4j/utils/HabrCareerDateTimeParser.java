6269381cc344b3ad16643009c717dd517992f83b
User <boris86msk@yandex.ru>
2022-11-02 21:41:08 +0300

    2.1. Преобразование даты /2

--- src/main/java/ru/job4j/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/utils/HabrCareerDateTimeParser.java
@@ -1,11 +1,11 @@
 package ru.job4j.utils;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
     @Override
     public LocalDateTime parse(String parse) {
-        parse = parse.substring(0, 19);
-        return LocalDateTime.parse(parse);
+        return ZonedDateTime.parse(parse).toLocalDateTime();
     }
 }
