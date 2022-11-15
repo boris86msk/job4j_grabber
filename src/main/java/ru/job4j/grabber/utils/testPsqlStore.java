133221b439ae878a7d2c802ce17752767a096439
User <boris86msk@yandex.ru>
2022-11-15 21:51:04 +0300

    Revert "5. PsqlStore /2"
    This reverts commit 0a84cc9e3aa52bacc7ea4a0be8cba40eab52e040.

--- src/main/java/ru/job4j/grabber/utils/testPsqlStore.java
+++ src/main/java/ru/job4j/grabber/utils/testPsqlStore.java
@@ -8,8 +8,8 @@ import java.io.InputStream;
 import java.util.List;
 import java.util.Properties;
 
-public class TestPsqlStore {
-    public static void main(String[] args) {
+public class testPsqlStore {
+    public static void main(String[] args) throws IOException {
         try (InputStream in = Store.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
             Properties config = new Properties();
             config.load(in);
@@ -26,8 +26,8 @@ public class TestPsqlStore {
 
             System.out.println(store.findById(20));
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
         }
     }
 }
